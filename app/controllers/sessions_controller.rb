7e3213773aa95760fbb7a9e4a4e8e8b3df90527c
Shaun <shaunhohoho@gmail.com>
2022-09-01 17:07:17 +0100

    sessions and redirect working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
-class LoginController < ApplicationController
+class SessionsController < ApplicationController
+
   def index
     @client_id = Rails.application.credentials.spotifive[:client_id]
-    @redirect_uri = "#{request.base_url}/login/callback"
+    @redirect_uri = sessions_callback_url
 
   end
 
   def callback
     code = params[:code]
     grant = Base64.strict_encode64("#{Rails.application.credentials.spotifive[:client_id]}:#{Rails.application.credentials.spotifive[:client_secret]}")
-    token = RestClient.post("https://accounts.spotify.com/api/token", {'grant_type': 'authorization_code', 'code': code, "redirect_uri": login_callback_url},{ 'Authorization': "Basic #{grant}"})
+    token = RestClient.post("https://accounts.spotify.com/api/token", {'grant_type': 'authorization_code', 'code': code, "redirect_uri": sessions_callback_url},{ 'Authorization': "Basic #{grant}"})
     token_json = JSON.parse(token)
     auth_token = token_json['access_token']
     refresh_token = token_json['refresh_token']
@@ -21,6 +22,16 @@ class LoginController < ApplicationController
     if !user
       user = User.create(email: user_email, name: user_name, image_url: user_img_url, auth_token: auth_token, refresh_token: refresh_token)
     end
-
+    session[:user] = user
+    redirect_to sessions_session_check_url
   end 
+
+  def session_check
+    print session[:user]
+  end 
+
+  def logout
+    
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'login#index'
-  get 'login/callback'
+  root 'sessions#index'
+  get 'sessions/callback'
+  get 'sessions/session_check'
   resources :games
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
