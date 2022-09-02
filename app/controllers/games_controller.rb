06287a243c8859adec871be3e9f1c825eeceb8a0
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-09-02 14:02:47 +0100

    ramdomised artist with id

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,14 @@ class GamesController < ApplicationController
   # GET /games or /games.json
   def index
     user = session[:user]
-    user_auth_token = user.auth_token
+    user_auth_token = user["auth_token"]
+    data = JSON.parse(RestClient.get("https://api.spotify.com/v1/playlists/0Hm1tCeFv45CJkNeIAtrfF?si=a87132e260a74789/tracks?tracks?fields=items(track(name%2Chref%2Calbum(name%2Chref)))", { 'Authorization': "Bearer #{user_auth_token}" }))
+    artists = data["tracks"]["items"]
+    artists.map! { |artist| { name: artist["track"]["album"]["artists"][0]["name"], id: artist["track"]["album"]["artists"][0]["id"] } }
+    random_artist = artists.sample
+    @artist_name = random_artist[:name]
+    top_tracks_data = JSON.parse(RestClient.get("https://api.spotify.com/v1/artists/6LuN9FCkKOj5PcnpouEgny/top-tracks?market=GB", { 'Authorization': "Bearer #{user_auth_token}", "Content-Type": "application/json", "Accept": "application/json" }))
+    print top_tracks_data[:tracks]
   end
 
   # GET /games/1 or /games/1.json
