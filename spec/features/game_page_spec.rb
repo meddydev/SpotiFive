061c0491ae08e040c4e7b1e33ea3d2dc4360f421
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-09-02 08:35:54 +0100

    Further Capybar feature tests done

--- spec/features/game_page_spec.rb
+++ spec/features/game_page_spec.rb
@@ -1,18 +1,23 @@
 require "rails_helper"
 
 RSpec.feature "Game Page", type: :feature do
+  scenario "should be successful" do
+    visit "/game"
+
+    click_button "Create User"
+    expect(page).to havecontent("")
+  end
+
   scenario "use fields to input game data" do
     visit "/game"
-    # click_link "New post"
-    # fill_in "Message", with: "Hello, world!"
+
     expect(page).has_field? ("1").to equal (true)
   end
 
   scenario "has name in field" do
     visit "/game"
-    # click_link "New post"
-    # fill_in "Message", with: "Hello, world!"
-    expect(page).has_field?("Name", with: "Drake"))
+
+    expect(page).has_field?("Name", with: "Drake")
   end
 
   scenario "Can submit button directing to results" do
