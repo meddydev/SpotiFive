d22096e21b9d9911e77352462cf2c793abb57e11
Irina Soldatenkova <irinavik1805@gmail.com>
2022-09-02 11:37:02 +0100

    Delete user can submit post

--- spec/features/user_can_submit_posts_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
-    visit "/scoreboard"
-    click_link "Return to Home Page"
-    expect(page).to have_content("Hello, world!")
-  end
-end
