c00fed2b010803639588ce0618b0c652a8b6b3a3
IrinaVi <irinavik1805@gmail.com>
2022-09-02 09:24:44 +0100

    Add request spec

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/scoreboard"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+    click_link "Return to Home Page"
     expect(page).to have_content("Hello, world!")
   end
 end
--- spec/requests/scoreboard_spec.rb
+++ spec/requests/scoreboard_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Scoreboards", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/scoreboard/index"
+      get "/scoreboard"
       expect(response).to have_http_status(:success)
     end
   end
