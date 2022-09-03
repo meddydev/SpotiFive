cf79ec80670ecf676b64373db1639c3cc90f0b65
Ahmed <ahmed@hallajian.co.uk>
2022-09-03 22:03:14 +0100

    added recent scores table to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
         if session[:user]
             @name = session[:user]["name"]
             @profile_picture_url = session[:user]["image_url"]
+            user_id = session[:user]["id"]
+            user_games = Game.where(user_id: user_id)
+            @recent_games = user_games.slice(0, 5)
+            p @recent_games
             p "IMAGE URL IS " + @profile_picture_url.to_s
         else
             redirect_to "/"
