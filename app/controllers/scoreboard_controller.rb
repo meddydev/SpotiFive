c2112153bf73527da57a67fcc163cf32d1ff0cd8
IrinaVi <irinavik1805@gmail.com>
2022-09-03 13:34:12 +0100

    Create hash with users and scores for the scoreboard

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -1,9 +1,20 @@
 class ScoreboardController < ApplicationController
   def index
-    if session[:user]
-      @users = User.all
-    else 
-      redirect_to "/"
+
+    @users = User.all
+    users_dict = []
+    @users.each do | user_object | 
+      one_user = Hash.new
+      one_user[:name] = user_object.name
+      one_user[:total_score] = user_object.total_score
+      one_user[:num_games] = user_object.num_games
+      if user_object.num_games > 0
+        one_user[:av_score] = user_object.total_score / user_object.num_games
+      else 
+        one_user[:av_score] = 0
+      end
+      users_dict << one_user
     end
+    puts "USERS:", users_dict, "USERS END"
   end
 end
