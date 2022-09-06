b1bb00c41fccc7aa8d14ed68a8a1b55a52683d01
Shaun <shaunhohoho@gmail.com>
2022-09-06 16:15:57 +0100

    refactored refresh_tokens and removed crap

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   get '/scoreboard', to: 'scoreboard#index'
   root 'sessions#index'
   get 'sessions/login'
-  get 'sessions/session_check'
   get 'sessions/logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'game', to: 'games#index'
   get 'home', to: 'home#index'
   get 'result', to: 'result#index'
-  resources :users
 end
