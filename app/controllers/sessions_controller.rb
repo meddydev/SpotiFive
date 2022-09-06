2773033475dafc2b4ae1f09a62c83f51aefe391c
IrinaVi <irinavik1805@gmail.com>
2022-09-06 07:55:41 +0100

    Fix a bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -36,7 +36,6 @@ class SessionsController < ApplicationController
 
   def logout
     reset_session
-    # # RestClient.get("https://accounts.spotify.com/logout")
     redirect_to "https://accounts.spotify.com/logout"
   end
 end
