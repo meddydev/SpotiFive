551726038493e62f80c46e3b7860ab0ef8da6776
Shaun <shaunhohoho@gmail.com>
2022-09-06 11:57:41 +0100

    results and scoring finally working !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -6,13 +6,17 @@ class ResultController < ApplicationController
     user_auth_token = session[:user]["auth_token"]
     artist_id = params[:artist_id]
     guesses = [params[:guess_1], params[:guess_2], params[:guess_3], params[:guess_4], params[:guess_5]]
-
+    guesses.map!{|input| self.input_formatter(input)}
     top_tracks_data = JSON.parse(RestClient.get("https://api.spotify.com/v1/artists/#{artist_id}/top-tracks?market=GB", { 'Authorization': "Bearer #{user_auth_token}", "Content-Type": "application/json", "Accept": "application/json" }))["tracks"]
     actuals = top_tracks_data[0,5].map{|track| track["name"]}
     
-    @results =self.get_score(guesses, actuals)
-
-    # @results = guess_formatted.zip(actual_formatted).map{|guess, actual| {guess: guess, actual: actual, score: self.get_score(guess,actual, actual_formatted,SONG_GUESS_SIMILARITY_THRESHOLD)}}
+    guesses_hash = Hash.new
+    actuals_hash = Hash.new
+    guesses.zip(actuals).each_with_index do |guesses_and_actuals,index|
+      guesses_hash[index] = guesses_and_actuals[0]
+      actuals_hash[index] = guesses_and_actuals[1]
+    end 
+    @results =self.get_score(guesses, actuals, guesses_hash, actuals_hash)
     @artist =JSON.parse(RestClient.get("https://api.spotify.com/v1/artists/#{artist_id}", { 'Authorization': "Bearer #{user_auth_token}", "Content-Type": "application/json", "Accept": "application/json" }))["name"]
 
   end 
@@ -20,67 +24,76 @@ class ResultController < ApplicationController
   private
 
 
-  def get_score(guesses, actuals)
-    actuals_hash = Hash.new
-    guesses_hash = Hash.new
-    actuals.each_with_index do |actual, index|
-      actuals_hash[index] = actual
-    end 
-    guesses.each_with_index do |guess, index|
-      if actuals.any?{|actual| self.song_name_formatter(actual).similar(self.song_name_formatter(guess)) > SONG_GUESS_SIMILARITY_THRESHOLD}
-        guesses_hash[index] = (actuals.select{|actual| self.song_name_formatter(actual).similar(self.song_name_formatter(guess)) > SONG_GUESS_SIMILARITY_THRESHOLD})[0]
-      end 
-    end 
+  def get_score(guesses, actuals, guesses_hash, actuals_hash)
     scores = Hash.new
-    print "\nXXXXXX\n"
-    print "EXACT MATCH"
-    print("\nactual hash : ", actuals_hash)
-    print("\nguess hash : ", guesses_hash)
-    print "\nXXXXXX\n"
-    actuals_hash.each do |key,value|
-      if guesses_hash[key] == actuals_hash[key]
-        scores[key] = 25
-        guesses_hash.delete(key)
-        actuals_hash.delete(key)
-      else
-        scores[key] = 0
-      end
-    end 
-    print "\nXXXXXX\n"
-    print "NEAR MATCH"
-    print("actual hash : ", actuals_hash)
-    print("\nguess hash : ", guesses_hash)
-    print "\nXXXXXX\n"
-    guesses_hash.each do |key,value|
-      print("\nkey", key)
-      print("\nvalue", value)
-      if actuals_hash.value?(value)
-        print("HAS VALUE")
-        scores[key] = 10
-        guesses_hash.delete(key)
-        actuals_hash.delete(actuals_hash.key(value))
-      end 
-    end 
-
-    print "\nXXXXXX\n"
-    print actuals_hash
-    print "\nXXXXXX\n"
+    self.get_perfect_match(guesses_hash,actuals_hash, scores)
+    self.get_close_match(guesses_hash,actuals_hash, scores)
     scores_array = []
     scores.each{ |index, score| scores_array.append({guess: guesses[index], actual: actuals[index], score: score})}
     return scores_array
   end 
 
+  def get_close_match(guesses_hash, actuals_hash, scores)
+    actuals_hash.each do |actual_index, actual_song|
+      scores_hash = Hash.new
+      guesses_hash.each do |guess_index, guess_song|
+        scores_hash[guess_index] = self.get_song_similarity(actual_song, guess_song)
+      end 
 
+      matching_song = scores_hash.filter{|index, score| score>SONG_GUESS_SIMILARITY_THRESHOLD }
+      if matching_song.length!=0
+        matching_song.select!{|k,v| v == matching_song.values.max }
+        scores[(matching_song.keys[0])] = 10
+        actuals_hash.delete(actual_index)
+        guesses_hash.delete(matching_song.key(matching_song.values.max))
+      end 
 
-  def song_name_formatter(song)
-    if song.include?("remix")
-      song_removed_hyphen = song
-    else 
-      song_removed_hyphen = song.sub /-.+/ ,""
     end 
-    song_removed_brackets = song_removed_hyphen.sub /\(.+/ , ""
-    return song_removed_brackets.strip.downcase
 
   end 
 
+
+  def get_perfect_match(guesses_hash,actuals_hash, scores)
+    actuals_hash.each do |index, song|
+      if self.get_song_similarity(song, guesses_hash[index]) > SONG_GUESS_SIMILARITY_THRESHOLD 
+        scores[index]=25 
+        guesses_hash.delete(index)
+        actuals_hash.delete(index)
+      else 
+        scores[index] = 0
+      end 
+    end 
+  end 
+
+
+  def song_name_formatter(song)
+    song_downcase = song.downcase
+    song_delatinised = ActiveSupport::Inflector.transliterate(song_downcase).to_s
+    # if song_delatinised.include?("remix")
+    #   song_formatted = song_delatinised.strip
+    # else 
+      song_removed_hyphen = song_delatinised.sub /-.+/ ,""
+      song_removed_brackets = song_removed_hyphen.sub /\(.+/ , ""
+      song_formatted = song_removed_brackets.strip
+    # end 
+    return song_formatted
+
+  end 
+
+  def input_formatter(input)
+    if input==nil
+      return ""
+    end
+    return input
+  end 
+
+  def get_song_similarity(song1, song2)
+    song_similarity = self.song_name_formatter(song1).similar(self.song_name_formatter(song2))
+    return song_similarity
+  end
+
+
+
 end
+
+
