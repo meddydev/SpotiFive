0fd1fd98a7cd48381abf36d8fd7bd76d7f6afdd3
Eghosa Edobor <eghosaedobor11@gmail.com>
2022-09-07 13:50:26 +0100

    gamepage user field failing test

--- spec/features/game_page_spec.rb
+++ spec/features/game_page_spec.rb
@@ -1,28 +1,39 @@
 require "rails_helper"
 
-RSpec.feature "Game Page", type: :feature do
-  scenario "should be successful" do
+RSpec.describe "Playing Game", type: :feature do
+  scenario "User Inputs" do
     visit "/game"
-
-    click_button "Create User"
-    expect(page).to havecontent("User	Artist	Artist name	Score")
+    fill_in "", with: ""
+    click_on "Submit"
+    visit "scoreboard"
+    expect(page).to have_content("")
   end
 
-  scenario "use fields to input game data" do
-    visit "/game"
+  #   scenario "end game abruptly" do
+  #     visit "/game"
+  #     fill_in "", with: ""
+  #     click_on "home"
+  #     visit "/home"
+  #     expect(page).to have_content("")
+  #   end
 
-    expect(page).has_field? ("1").to equal (true)
-  end
+  # end
 
-  scenario "has name in field" do
-    visit "/game"
+  # scenario "use fields to input game data" do
+  #   visit "/game"
 
-    expect(page).has_field?("Name", with: "Drake")
-  end
+  #   expect(page).has_field? ("1").to equal (true)
+  # end
 
-  scenario "Can submit button directing to results" do
-    visit "/game"
+  # scenario "has name in field" do
+  #   visit "/game"
 
-    expect(page).has_button?("Submit")
-  end
+  #   expect(page).has_field?("Name", with: "Drake")
+  # end
+
+  # scenario "Can submit button directing to results" do
+  #   visit "/game"
+
+  #   expect(page).has_button?("Submit")
+  # end
 end
