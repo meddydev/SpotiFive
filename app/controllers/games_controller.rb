36cfeaca884205641d5a02205a27a2ee993b4e26
Shaun <shaunhohoho@gmail.com>
2022-09-07 14:40:58 +0100

    added session update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,7 @@ class GamesController < ApplicationController
   # GET /games or /games.json
 
   def guess_the_songs
-    if session[:user]
+    if session[:id]
       user = User.find_by(id: session[:id])
       refresh_tokens(user.refresh_token)
       user = User.find_by(id: session[:id])
@@ -24,16 +24,21 @@ class GamesController < ApplicationController
   end
 
   def guess_the_artist
-    user = session[:user]
-    refresh_tokens(session[:user]["refresh_token"])
-    user_auth_token = user["auth_token"]
-    data = JSON.parse(RestClient.get("https://api.spotify.com/v1/playlists/0Hm1tCeFv45CJkNeIAtrfF", { 'Authorization': "Bearer #{user_auth_token}", "Content-Type": "application/json", "Accept": "application/json" }))
-    artists = data["tracks"]["items"]
-    artists.map! { |artist| { name: artist["track"]["album"]["artists"][0]["name"], id: artist["track"]["album"]["artists"][0]["id"] } }
-    random_artist = artists.sample
-    @artist = random_artist
-    top_tracks_data = JSON.parse(RestClient.get("https://api.spotify.com/v1/artists/#{random_artist[:id]}/top-tracks?market=GB", { 'Authorization': "Bearer #{user_auth_token}", "Content-Type": "application/json", "Accept": "application/json" }))["tracks"]
-    @top_five_tracks = top_tracks_data.sample(5).map { |track| track["name"].gsub(@artist[:name], "'THE ARTIST'S NAME'") }
+    if session[:id]
+      user = User.find_by(id: session[:id])
+      refresh_tokens(user.refresh_token)
+      user = User.find_by(id: session[:id])
+      user_auth_token = user.auth_token
+      data = JSON.parse(RestClient.get("https://api.spotify.com/v1/playlists/0Hm1tCeFv45CJkNeIAtrfF", { 'Authorization': "Bearer #{user_auth_token}", "Content-Type": "application/json", "Accept": "application/json" }))
+      artists = data["tracks"]["items"]
+      artists.map! { |artist| { name: artist["track"]["album"]["artists"][0]["name"], id: artist["track"]["album"]["artists"][0]["id"] } }
+      random_artist = artists.sample
+      @artist = random_artist
+      top_tracks_data = JSON.parse(RestClient.get("https://api.spotify.com/v1/artists/#{random_artist[:id]}/top-tracks?market=GB", { 'Authorization': "Bearer #{user_auth_token}", "Content-Type": "application/json", "Accept": "application/json" }))["tracks"]
+      @top_five_tracks = top_tracks_data.sample(5).map { |track| track["name"].gsub(@artist[:name], "'THE ARTIST'S NAME'") }
+    else
+      redirect_to "/"
+    end
   end
 
 
